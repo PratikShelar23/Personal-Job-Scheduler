8817a61139c2d41619dc1eeda13104f936b95617
PratikShelar23 <119117359+PratikShelar23@users.noreply.github.com>
2023-12-30 15:31:43 +0530

    Delete dtroy_job directory

--- dtroy_job/main/job/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Myreg,resume
-
-admin.site.register(Myreg)
-admin.site.register(resume)
-
--- dtroy_job/main/job/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from job.models import Myreg
-
-class Company(forms.ModelForm):
-    class Meta:
-        model = Myreg
-        fields = ['companyName', 'driveDate', 'link']
